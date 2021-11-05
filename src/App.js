f4a2bad06e067b498f942db9077055822672a872
Duong <Duongchauv@gmail.com>
2021-11-05 00:34:44 -0500

    Changed browser router to hashrouter for refresh functionality on gh pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import React from 'react';
 import Home from './Components/Home';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {HashRouter as Router, Switch, Route} from 'react-router-dom';
 import NavBar from './Components/NavBar';
 import CreatePost from './Components/CreatePost';
 import ViewPost from './Components/ViewPost';
